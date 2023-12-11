63b31a4db4a18e96b97ddda5fceb57143fa879b7
MariiaSam <kontragent@ukr.net>
2023-12-11 12:37:19 +0200

    corr fetch

--- src/serviсes/getImages.js
+++ src/serviсes/getImages.js
@@ -4,10 +4,6 @@ const API_KEY = '41152283-58ec30cc31062511e016aee88';
 const BASE_URL = 'https://pixabay.com/api';
 axios.defaults.baseURL = BASE_URL;
 
-axios.defaults.params = {
-  orientation: 'horizontal',
-  per_page: 12,
-};
 
 export const getImages = async (searchQuery, page) => {
   const { data } = await axios.get(
